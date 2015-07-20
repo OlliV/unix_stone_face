90151a23fcb9ff605449336e8ec81bde01c92208
Olli Vanhoja <olli.vanhoja@gmail.com>
2015-07-21 00:00:46 +0300

    unix ts, local time and

--- src/main.c
+++ src/main.c
@@ -4,9 +4,10 @@
 static Window *main_window;
 static TextLayer *utime_layer;
 static TextLayer *time_layer[2];
+static TextLayer *date_layer;
 static GFont time_font;
 
-#define TIMEVLEN    11
+#define TIMEVLEN    20
 #define TIMEFMT_12H "%I:%M %p"
 #define TIMEFMT_24H "%H:%M"
 
@@ -15,26 +16,34 @@ static void update_time(void)
     static char utime_buffer[TIMEVLEN];
     static char ltime_buffer[TIMEVLEN];
     static char ptime_buffer[TIMEVLEN];
-    time_t utime;
-    time_t ltime;
-    time_t ptime;
+    static char date_buffer[TIMEVLEN];
+    time_t utime, ptime;
+    struct tm *tm;
 
     utime = time(NULL);
-    ltime = localtime(&temp);
-    ptime = ltime - 10 * 60 * 60;
-
+    ptime = utime - 10 * 60 * 60;
+    
     snprintf(utime_buffer, TIMEVLEN, "%x", (int)utime);
+    
+    tm = localtime(&utime);
+    strftime(date_buffer, TIMEVLEN, "%a %b %e", tm);
+    
     if(clock_is_24h_style() == true) {
-        strftime(ltime_buffer, TIMEVLEN, TIMEFMT_24H, ltime);
-        strftime(ptime_buffer, TIMEVLEN, TIMEFMT_24H, ptime);
+        strftime(ltime_buffer, TIMEVLEN, TIMEFMT_24H, tm);
+        
+        tm = localtime(&ptime);
+        strftime(ptime_buffer, TIMEVLEN, TIMEFMT_24H, tm);
     } else {
-        strftime(ltime_buffer, TIMEVLEN, TIMEFMT_12H, ltime);
-        strftime(ptime_buffer, TIMEVLEN, TIMEFMT_12H, ptime);
+        strftime(ltime_buffer, TIMEVLEN, TIMEFMT_12H, tm);
+        
+        tm = localtime(&ptime);
+        strftime(ptime_buffer, TIMEVLEN, TIMEFMT_12H, tm);
     }
 
     text_layer_set_text(utime_layer, utime_buffer);
-    text_layer_set_text(time_layer[0], utime_buffer);
+    text_layer_set_text(time_layer[0], ltime_buffer);
     text_layer_set_text(time_layer[1], ptime_buffer);
+    text_layer_set_text(date_layer, date_buffer);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed)
@@ -48,32 +57,69 @@ static void main_window_load(Window *window)
 
     time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_23));
 
+    /*
+     * Unix timestamp.
+     */
+    
     utime_layer = text_layer_create(GRect(5, 5, 139, 50));
     text_layer_set_background_color(utime_layer, GColorClear);
     text_layer_set_text_color(utime_layer, GColorBlack);
     text_layer_set_text(utime_layer, "");
+    
+    text_layer_set_font(utime_layer, time_font);
+    text_layer_set_text_alignment(utime_layer, GTextAlignmentCenter);
 
-    for (i = 0; i < sizeof(time_layer) / sizeof(time_layer[0]; i++)) {
+    layer_add_child(window_get_root_layer(window), text_layer_get_layer(utime_layer));
+    
+    /*
+     * Time.
+     */
+    for (i = 0; i < sizeof(time_layer) / sizeof(time_layer[0]); i++) {
         static TextLayer *tl;
 
-        time_layer[i] = text_layer_create(GRect(5, 5 + i * 23, 139, 55 + i * 23));
+        time_layer[i] = text_layer_create(GRect(5, 35 + i * 23, 139, 90 + i * 23));
         tl = time_layer[i];
 
         text_layer_set_background_color(tl, GColorClear);
         text_layer_set_text_color(tl, GColorBlack);
         text_layer_set_text(tl, "00:00");
+        
+        text_layer_set_font(tl, time_font);
+        text_layer_set_text_alignment(tl, GTextAlignmentCenter);
+        
+        layer_add_child(window_get_root_layer(window), text_layer_get_layer(tl));
     }
+    
+    /*
+     * Date.
+     */
+    time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_23));
 
-    text_layer_set_font(time_layer, time_font);
-    text_layer_set_text_alignment(time_layer, GTextAlignmentCenter);
+    date_layer = text_layer_create(GRect(5, 35 + 3 * 23, 139, 3 * 23));
+    text_layer_set_background_color(date_layer, GColorClear);
+    text_layer_set_text_color(date_layer, GColorBlack);
+    text_layer_set_text(date_layer, "");
+    
+    text_layer_set_font(date_layer, time_font);
+    text_layer_set_text_alignment(date_layer, GTextAlignmentCenter);
+    
+    layer_add_child(window_get_root_layer(window), text_layer_get_layer(date_layer));
 
-    layer_add_child(window_get_root_layer(window), text_layer_get_layer(time_layer));
     update_time();
 }
 
 static void main_window_unload(Window *window)
 {
-    text_layer_destroy(time_layer);
+    size_t i;
+    
+    text_layer_destroy(utime_layer);
+    
+    for (i = 0; i < sizeof(time_layer) / sizeof(time_layer[0]); i++) {
+        text_layer_destroy(time_layer[i]);
+    }
+    
+    text_layer_destroy(date_layer);
+    
     fonts_unload_custom_font(time_font);
 }
 
@@ -88,7 +134,7 @@ static void init(void)
 
     window_stack_push(main_window, true);
 
-    tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
+    tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 }
 
 static void deinit(void)

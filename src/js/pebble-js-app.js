9eb2eab9139ec729561a1f01e5876cae44d4b545
Olli Vanhoja <olli.vanhoja@gmail.com>
2015-07-21 23:02:12 +0300

    weather

---
+++ src/js/pebble-js-app.js
@@ -0,0 +1,76 @@
+function iconFromWeatherId(weatherId) {
+    if (weatherId < 600) {
+        return 2;
+    } else if (weatherId < 700) {
+        return 3;
+    } else if (weatherId > 800) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+var xhrRequest = function (url, type, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+        callback(this.responseText);
+    };
+    xhr.open(type, url);
+    xhr.send();
+};
+
+function locationSuccess(pos) {
+     // Construct URL
+    var url = "http://api.openweathermap.org/data/2.5/weather?lat=" +
+        pos.coords.latitude + "&lon=" + pos.coords.longitude;
+
+    // Send request to OpenWeatherMap
+    xhrRequest(url, 'GET', function(responseText) {
+        // responseText contains a JSON object with weather info
+        var json = JSON.parse(responseText);
+
+        // Temperature in Kelvin requires adjustment
+        var temperature = Math.round(json.main.temp - 273.15);
+        var conditions = json.weather[0].main;      
+        var icon = iconFromWeatherId(json.weather[0].id);
+      
+        // Assemble dictionary using our keys
+        var dictionary = {
+            "KEY_TEMPERATURE": temperature,
+            "KEY_CONDITIONS": conditions,
+            "WEATHER_ICON_KEY": icon
+        };
+
+        // Send to Pebble
+        Pebble.sendAppMessage(dictionary, function(e) {
+                console.log("Weather info sent to Pebble successfully!");
+            }, function(e) {
+                console.log("Error sending weather info to Pebble!");
+            });
+    });
+}
+
+function locationError(err) {
+    console.log("Error requesting location!");
+}
+
+function getWeather() {
+    navigator.geolocation.getCurrentPosition(
+        locationSuccess,
+        locationError,
+        {timeout: 15000, maximumAge: 60000});
+}
+
+// Listen for when the watchface is opened
+Pebble.addEventListener('ready', function(e) {
+    console.log("PebbleKit JS ready!");
+    
+    // Get the initial weather
+    getWeather();
+});
+
+// Listen for when an AppMessage is received
+Pebble.addEventListener('appmessage', function(e) {
+    console.log("AppMessage received!");
+    getWeather();
+});
--- src/main.c
+++ src/main.c
@@ -4,18 +4,32 @@
 static Window *main_window;
 static TextLayer *utime_layer;
 static TextLayer *time_layer;
+static TextLayer *weather_layer;
 static TextLayer *date_layer;
 static TextLayer *status_layer;
 static GFont time_font;
+static BitmapLayer *icon_layer;
+static GBitmap *icon_bitmap = NULL;
 
 static char *bt_status = "Unk";
 static char batt_status[] = "+100%";
 
-#define TIMEVLEN    9
-#define TIMEFMT_12H "%I:%M %p"
-#define TIMEFMT_24H "%H:%M"
-#define DATEVLEN    11
-#define DATEFMT     "%a %b %e"
+#define TIMEVLEN            9
+#define TIMEFMT_12H         "%I:%M %p"
+#define TIMEFMT_24H         "%H:%M"
+#define DATEVLEN            11
+#define DATEFMT             "%a %b %e"
+
+#define KEY_TEMPERATURE     0
+#define KEY_CONDITIONS      1
+#define WEATHER_ICON_KEY    2
+
+static const uint32_t WEATHER_ICONS[] = {
+  RESOURCE_ID_IMAGE_SUN,
+  RESOURCE_ID_IMAGE_CLOUD,
+  RESOURCE_ID_IMAGE_RAIN,
+  RESOURCE_ID_IMAGE_SNOW
+};
 
 static void update_time(void)
 {
@@ -30,7 +44,7 @@ static void update_time(void)
 
     snprintf(utime_buffer, sizeof(utime_buffer), "%x", (unsigned)utime);
 
-    if(clock_is_24h_style() == true) {
+    if (clock_is_24h_style() == true) {
         strftime(time_buffer, sizeof(time_buffer), TIMEFMT_24H, tm);
     } else {
         strftime(time_buffer, sizeof(time_buffer), TIMEFMT_12H, tm);
@@ -70,6 +84,68 @@ static void handle_battery(BatteryChargeState charge_state)
     update_status();
 }
 
+static void inbox_received_callback(DictionaryIterator *iterator, void *context)
+{
+    /* Store incoming information */
+    static char temperature_buffer[8];
+    static char conditions_buffer[32];
+    static char weather_layer_buffer[32];
+  
+    Tuple *t = dict_read_first(iterator); 
+    while(t != NULL) {
+        // Which key was received?
+        switch (t->key) {
+        case KEY_TEMPERATURE:
+            snprintf(temperature_buffer, sizeof(temperature_buffer),
+                     "%dC", (int)t->value->int32);
+            break;
+        case KEY_CONDITIONS:
+            snprintf(conditions_buffer, sizeof(conditions_buffer),
+                     "%s", t->value->cstring);
+            break;
+        case WEATHER_ICON_KEY:
+            if (icon_bitmap) {
+                gbitmap_destroy(icon_bitmap);
+            }
+
+            icon_bitmap = gbitmap_create_with_resource(WEATHER_ICONS[t->value->uint8]);
+
+#ifdef PBL_SDK_3
+            bitmap_layer_set_compositing_mode(icon_layer, GCompOpSet);
+#endif
+            bitmap_layer_set_bitmap(icon_layer, icon_bitmap);
+            break;
+        default:
+            APP_LOG(APP_LOG_LEVEL_ERROR, "Key %d not recognized!", (int)t->key);
+            break;
+        }
+
+        /* Look for next item */
+        t = dict_read_next(iterator);
+    }
+  
+    /* Assemble full string and display */
+    snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s, %s",
+             temperature_buffer, conditions_buffer);
+    text_layer_set_text(weather_layer, weather_layer_buffer);
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context)
+{
+    APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator,
+                                   AppMessageResult reason, void *context)
+{
+    APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context)
+{
+    APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
+
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed)
 {
     handle_bluetooth(bluetooth_connection_service_peek());
@@ -77,6 +153,13 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed)
 
     update_time();
     update_status();
+
+    if (tick_time->tm_min % 60 == 0) {
+        DictionaryIterator *iter;
+        app_message_outbox_begin(&iter);
+        dict_write_uint8(iter, 0, 0);
+        app_message_outbox_send();
+    }
 }
 
 static void main_window_load(Window *window)
@@ -91,24 +174,38 @@ static void main_window_load(Window *window)
     /*
      * Unix timestamp.
      */
-    utime_layer = text_layer_create(GRect(0, 2, bounds.size.w, 24));
+    utime_layer = text_layer_create(GRect(0, 2, bounds.size.w, 30));
     text_layer_set_background_color(utime_layer, GColorClear);
     text_layer_set_text_color(utime_layer, GColorBlack);
-    text_layer_set_text(utime_layer, "");
     text_layer_set_font(utime_layer, time_font);
     text_layer_set_text_alignment(utime_layer, GTextAlignmentCenter);
+    text_layer_set_text(utime_layer, "");
     layer_add_child(window_get_root_layer(window), text_layer_get_layer(utime_layer));
 
     /*
      * Time.
      */
-    time_layer = text_layer_create(GRect(0, 30, bounds.size.w, 24));
+    time_layer = text_layer_create(GRect(0, 30, bounds.size.w, 30));
     text_layer_set_background_color(time_layer, GColorClear);
     text_layer_set_text_color(time_layer, GColorBlack);
-    text_layer_set_text(time_layer, "00:00");
     text_layer_set_font(time_layer, time_font);
     text_layer_set_text_alignment(time_layer, GTextAlignmentCenter);
+    text_layer_set_text(time_layer, "00:00");
     layer_add_child(window_get_root_layer(window), text_layer_get_layer(time_layer));
+
+    /*
+     * Weather.
+     */
+    weather_layer = text_layer_create(GRect(0, 95, bounds.size.w, 30));
+    text_layer_set_background_color(weather_layer, GColorClear);
+    text_layer_set_text_color(weather_layer, GColorBlack);
+    text_layer_set_font(weather_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
+    text_layer_set_text_alignment(weather_layer, GTextAlignmentCenter);
+    text_layer_set_text(weather_layer, "?");
+    layer_add_child(window_get_root_layer(window), text_layer_get_layer(weather_layer));
+    
+    icon_layer = bitmap_layer_create(GRect(32, 45, 64, 64));
+    layer_add_child(window_layer, bitmap_layer_get_layer(icon_layer));
 
     /*
      * Date.
@@ -116,9 +213,9 @@ static void main_window_load(Window *window)
     date_layer = text_layer_create(GRect(0, bounds.size.h - 50, bounds.size.w, 24));
     text_layer_set_background_color(date_layer, GColorClear);
     text_layer_set_text_color(date_layer, GColorBlack);
-    text_layer_set_text(date_layer, "");
     text_layer_set_font(date_layer, time_font);
     text_layer_set_text_alignment(date_layer, GTextAlignmentCenter);
+    text_layer_set_text(date_layer, "");
     layer_add_child(window_get_root_layer(window), text_layer_get_layer(date_layer));
 
     /*
@@ -143,9 +240,15 @@ static void main_window_unload(Window *window)
 {
     text_layer_destroy(utime_layer);
     text_layer_destroy(time_layer);
+    text_layer_destroy(weather_layer);
+    if (icon_bitmap) {
+        gbitmap_destroy(icon_bitmap);
+    }
     text_layer_destroy(date_layer);
+    text_layer_destroy(status_layer);
 
     fonts_unload_custom_font(time_font);
+    bitmap_layer_destroy(icon_layer);
 }
 
 static void init(void)
@@ -160,6 +263,17 @@ static void init(void)
     window_stack_push(main_window, true);
 
     tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+    
+    /*
+     * Register message callbacks.
+     */
+    app_message_register_inbox_received(inbox_received_callback);
+    app_message_register_inbox_dropped(inbox_dropped_callback);
+    app_message_register_outbox_failed(outbox_failed_callback);
+    app_message_register_outbox_sent(outbox_sent_callback);
+
+    app_message_open(app_message_inbox_size_maximum(),
+                     app_message_outbox_size_maximum());
 }
 
 static void deinit(void)
